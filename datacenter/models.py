06292c904bb1169040e6a1a5ea66785f45daf30a
Dtlama <serebryakovss1901@gmail.com>
2023-01-21 17:58:39 +0300

    upload website information

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Passcard(models.Model):
@@ -28,3 +29,20 @@ class Visit(models.Model):
                 if self.leaved_at else 'not leaved'
             )
         )
+
+
+def get_visit_duration(visit):
+    if visit.leaved_at:
+        visit_duration = visit.leaved_at - visit.enterned_at
+        return visit_duration
+    else:
+        print(visit.entered_at, timezone.now())
+        visit_duration = timezone.now() - visit.entered_at
+        return visit_duration
+
+def format_duration(timedelta):
+    total_seconds = timedelta.total_seconds()
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+    seconds = total_seconds % 60
+    return f'{int(hours)}:{int(minutes)}:{int(seconds)}'
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,22 @@
 from datacenter.models import Passcard
-from datacenter.models import Visit
+from datacenter.models import Visit, format_duration, get_visit_duration
 from django.shortcuts import render
+from django.utils import timezone
 
 
 def storage_information_view(request):
     # Программируем здесь
+    visits = Visit.objects.filter(leaved_at=None)
+    non_closed_visits = []
 
+    for visit in visits:
+        non_closed_visit = {
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': visit.entered_at,
+            'duration': format_duration(get_visit_duration(visits[0])),
+            }
+        non_closed_visits.append(non_closed_visit)
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
